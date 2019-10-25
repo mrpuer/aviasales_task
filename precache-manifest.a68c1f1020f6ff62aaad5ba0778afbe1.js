dc823f59922a08aae2a42691fbd43874df780a87
Vladimir <vova@mytnik.net>
2019-10-25 16:51:52 +0300

    Updates

---
+++ precache-manifest.a68c1f1020f6ff62aaad5ba0778afbe1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccdcd14a93f7e3a5d95f46262387db08",
+    "url": "/jm_aviasales/index.html"
+  },
+  {
+    "revision": "362c9614a33d0c0849fc",
+    "url": "/jm_aviasales/static/js/2.782db844.chunk.js"
+  },
+  {
+    "revision": "597c846d179b3318a09d",
+    "url": "/jm_aviasales/static/js/main.5037f24c.chunk.js"
+  },
+  {
+    "revision": "b862021a7a3c4b2f93a8",
+    "url": "/jm_aviasales/static/js/runtime-main.7f794d36.js"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/jm_aviasales/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/jm_aviasales/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/jm_aviasales/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/jm_aviasales/static/media/logo.0c089234.svg"
+  }
+]);
