1117aeebac07be3ecfcc7d91f8e9de5edd65f8db
puer <vova@mytnik.net>
2020-02-25 12:01:51 +0300

    Updates

---
+++ precache-manifest.f08cdce69f7f71ed40f5e71bf1666c9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551f7fcd658469036ac2a6b53e2b4b6d",
+    "url": "/aviasales_task/index.html"
+  },
+  {
+    "revision": "17ef302776a2bfc75dd2",
+    "url": "/aviasales_task/static/js/2.16a6ec6c.chunk.js"
+  },
+  {
+    "revision": "93c3836f719b19324ecf",
+    "url": "/aviasales_task/static/js/main.d85d3ca3.chunk.js"
+  },
+  {
+    "revision": "774d5c0862e41b85fa3e",
+    "url": "/aviasales_task/static/js/runtime-main.8b410c78.js"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/aviasales_task/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/aviasales_task/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/aviasales_task/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/aviasales_task/static/media/logo.0c089234.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jm_aviasales/precache-manifest.96dbe73d51639d91de29388f5a1ae1d5.js"
+  "/aviasales_task/precache-manifest.f08cdce69f7f71ed40f5e71bf1666c9b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jm_aviasales/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aviasales_task/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
